619e1cdc68bc9a42f9222debaeee24390b4cc3bd
RubyDragoon7197 <Andrespedrozab2004@gmail.com>
2025-09-22 01:50:27 +0000

    final

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Base path para GitHub Pages
+  base: '/rompecabezas/', 
+  server: {
+    open: true, // abre automáticamente el navegador al iniciar dev
+  },
+  build: {
+    outDir: 'dist', // carpeta de salida, coincidiendo con gh-pages
+  }
 })
